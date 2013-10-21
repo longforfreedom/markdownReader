3fa27fad8d872dbc6d03f415b7e04aed2d8b623e
migle <longforfreedom@gmail.com>
2013-10-21 12:21:14 +0800

    Update showdown.js
    添加githup.js,table.js中的内容到一个文件中

--- showdown.js
+++ showdown.js
@@ -991,7 +991,7 @@ var _DoCodeBlocks = function(text) {
 			codeblock = codeblock.replace(/^\n+/g,""); // trim leading newlines
 			codeblock = codeblock.replace(/\n+$/g,""); // trim trailing whitespace
 
-			codeblock = '<pre class="prettyprint linenums"><code>' + codeblock + "\n</code></pre>";
+			codeblock = "<pre><code>" + codeblock + "\n</code></pre>";
 
 			return hashBlock(codeblock) + nextChar;
 		}
@@ -1452,3 +1452,118 @@ if (typeof define === 'function' && define.amd) {
         return Showdown;
     });
 }
+
+// github
+(function(){
+    var github = function(converter) {
+        return [
+            {
+              // strike-through
+              // NOTE: showdown already replaced "~" with "~T", so we need to adjust accordingly.
+              type    : 'lang',
+              regex   : '(~T){2}([^~]+)(~T){2}',
+              replace : function(match, prefix, content, suffix) {
+                  return '<del>' + content + '</del>';
+              }
+            }
+        ];
+    };
+
+    // Client-side export
+    if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { window.Showdown.extensions.github = github; }
+    // Server-side export
+    if (typeof module !== 'undefined') module.exports = github;
+}());
+
+
+// table
+(function(){
+  var table = function(converter) {
+    var tables = {}, style = 'text-align:left;', filter; 
+    tables.th = function(header){
+      if (header.trim() === "") { return "";}
+      var id = header.trim().replace(/ /g, '_').toLowerCase();
+      return '<th id="' + id + '" style="'+style+'">' + header + '</th>';
+    };
+    tables.td = function(cell) {
+      return '<td style="'+style+'">' + converter.makeHtml(cell) + '</td>';
+    };
+    tables.ths = function(){
+      var out = "", i = 0, hs = [].slice.apply(arguments);
+      for (i;i<hs.length;i+=1) {
+        out += tables.th(hs[i]) + '\n';
+      }
+      return out;
+    };
+    tables.tds = function(){
+      var out = "", i = 0, ds = [].slice.apply(arguments);
+      for (i;i<ds.length;i+=1) {
+        out += tables.td(ds[i]) + '\n';
+      }
+      return out;
+    };
+    tables.thead = function() {
+      var out, i = 0, hs = [].slice.apply(arguments);
+      out = "<thead>\n";
+      out += "<tr>\n";
+      out += tables.ths.apply(this, hs);
+      out += "</tr>\n";
+      out += "</thead>\n";
+      return out;
+    };
+    tables.tr = function() {
+      var out, i = 0, cs = [].slice.apply(arguments);
+      out = "<tr>\n";
+      out += tables.tds.apply(this, cs);
+      out += "</tr>\n";
+      return out;
+    };
+    filter = function(text) { 
+      var i=0, lines = text.split('\n'), tbl = [], line, hs, rows, out = [];
+      for (i; i<lines.length;i+=1) {
+        line = lines[i];
+        // looks like a table heading
+        if (line.trim().match(/^[|]{1}.*[|]{1}$/)) {
+          line = line.trim();
+          tbl.push('<table>');
+          hs = line.substring(1, line.length -1).split('|');
+          tbl.push(tables.thead.apply(this, hs));
+          line = lines[++i];
+          if (!line.trim().match(/^[|]{1}[-=| ]+[|]{1}$/)) {
+            // not a table rolling back
+            line = lines[--i];
+          }
+          else {
+            line = lines[++i];
+            tbl.push('<tbody>');
+            while (line.trim().match(/^[|]{1}.*[|]{1}$/)) {
+              line = line.trim();
+              tbl.push(tables.tr.apply(this, line.substring(1, line.length -1).split('|')));
+              line = lines[++i];
+            }
+            tbl.push('</tbody>');
+            tbl.push('</table>');
+            // we are done with this table and we move along
+            out.push(tbl.join('\n'));
+            continue;
+          }
+        }
+        out.push(line);
+      }             
+      return out.join('\n');
+    };
+    return [
+    { 
+      type: 'lang', 
+      filter: filter
+    }
+    ];
+  };
+
+  // Client-side export
+  if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) { window.Showdown.extensions.table = table; }
+  // Server-side export
+  if (typeof module !== 'undefined') {
+    module.exports = table;
+  }
+}());
